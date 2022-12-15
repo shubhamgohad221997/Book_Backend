a88043fe8537e0e073c6b417d321d8fc1ed919c5
shubhamgohad221997 <shubhamgohad221997@gmail.com>
2022-12-15 14:46:24 +0530

    A2

--- Connect/Connect.js
+++ Connect/Connect.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
 const connect = () => {
-    return mongoose.connect("Mock-server:Mock-server@cluster0.an24sqd.mongodb.net/shubham?retryWrites=true&w=majority")
+    return mongoose.connect("mongodb+srv://Mock-server:Mock-server@cluster0.an24sqd.mongodb.net/shubham?retryWrites=true&w=majority")
 }
 module.exports = connect
